1f856b6abb2dfc438e7907c24a00243e8b374ec5
Bruno Sales <bruno.m.sales03@gmail.com>
2023-11-07 11:21:10 -0300

    (add) added a function that calculates the median point of all created points.

--- source.c
+++ source.c
@@ -8,7 +8,6 @@ typedef struct Point{
 } Point;
 
 float Random_Number(){
-	srand(clock());
 	int integer;
 	float whole;
 	float decimal;
@@ -17,39 +16,65 @@ float Random_Number(){
 	decimal = (float)integer;
 	decimal /= 1000;
 
-	integer = rand() % 501;
+	integer = rand() % 500;
 	whole = (float)integer;
 
 	return whole + decimal;
 }
 
-
-void Array_Define(int size, struct Point **ptr){
-	(*ptr) = malloc(size * sizeof(*(*ptr)));
+void Point_Median(int size, struct Point* points, struct Point* median){
+	float sum_x = 0;
+	float sum_y = 0;
+	for(int i = 0; i < size; i++){
+		sum_x += points[i].x;
+		sum_y += points[i].y;
+	}
+	median->x = sum_x / size;
+	median->y = sum_y / size;
 }
 
-void Array_Fill_Random(int size, struct Point *ptr){
+void Point_Transpose(int size, struct Point* points, struct Point* median){
 	for(int i = 0; i < size; i++){
-		ptr[i].x = Random_Number();
-		ptr[i].y = Random_Number();
+		points[i].x -= median->x;
+		points[i].y -= median->y;
+	}
+	median->x -= median->x;
+	median->y -= median->y;
+}
+
+void Array_Define(int size, struct Point** points){
+	(*points) = malloc(size * sizeof(*(*points)));
+}
+
+void Array_Fill_Random(int size, struct Point* points){
+	for(int i = 0; i < size; i++){
+		points[i].x = Random_Number();
+		points[i].y = Random_Number();
 	}
 }
 
-void Array_Display(int size, struct Point *ptr){
+void Array_Display(int size, struct Point* points){
 	for(int i = 0; i < size; i++){
-		printf("%d- %f %f\n", i + 1, ptr[i].x, ptr[i].y);
+		printf("%d- (%f, %f)\n", i + 1, points[i].x, points[i].y);
 	}
 }
 	
 int main(){
-	Point *ptr;
+	srand(clock());
+	Point* points;
+	Point median;
 	int size;
-
 	printf("How many random points do you want to create?\n");
 	scanf("%d", &size);
 
-	Array_Define(size, &ptr);
-	Array_Fill_Random(size, ptr);
-	Array_Display(size, ptr);
-	free(ptr);
+	Array_Define(size, &points);
+	Array_Fill_Random(size, points);
+	Point_Median(size, points, &median);
+	Array_Display(size, points);
+	Point_Transpose(size, points, &median);
+	Array_Display(size, points);
+	free(points);
 }
+/* TODO:
+ * calculate the angles of every point
+ * */
