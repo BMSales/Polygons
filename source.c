ce8dce949184385cb2b1d82f6f2cd6f2939dc197
Bruno Sales <bruno.m.sales03@gmail.com>
2023-11-09 10:26:01 -0300

    (add) Added functions that find the angle of every point relative to a predefined unit vector <1, 0>

--- source.c
+++ source.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <math.h>
 
 typedef struct Point{
 	float x;
 	float y;
+	float angle;
 } Point;
+
+//Function takes a vector and calculates its angle in relation to the unit vector <1, 0>
+void Calculate_Angle(struct Point *vector){
+	if(vector->y >= 0){
+		vector->angle = acos( (vector->x) / sqrt( pow(vector->x, 2.0) + pow(vector->y, 2.0) ) ) * 180/M_PI;
+	}else{
+		vector->angle = -1 * ( (acos( (vector->x) / sqrt( pow(vector->x, 2.0) + pow(vector->y, 2.0) ) ) * 180/M_PI) - 360);
+	}
+}
 
 float Random_Number(){
 	int integer;
@@ -22,7 +32,7 @@ float Random_Number(){
 	return whole + decimal;
 }
 
-void Point_Median(int size, struct Point* points, struct Point* median){
+void Point_Median(int size, struct Point *points, struct Point *median){
 	float sum_x = 0;
 	float sum_y = 0;
 	for(int i = 0; i < size; i++){
@@ -33,7 +43,7 @@ void Point_Median(int size, struct Point* points, struct Point* median){
 	median->y = sum_y / size;
 }
 
-void Point_Transpose(int size, struct Point* points, struct Point* median){
+void Point_Transpose(int size, struct Point *points, struct Point *median){
 	for(int i = 0; i < size; i++){
 		points[i].x -= median->x;
 		points[i].y -= median->y;
@@ -42,26 +52,34 @@ void Point_Transpose(int size, struct Point* points, struct Point* median){
 	median->y -= median->y;
 }
 
-void Array_Define(int size, struct Point** points){
+void Point_Define_Angles(int size, struct Point *points){
+	for(int i = 0; i < size; i++){
+		Calculate_Angle(&points[i]);
+	}
+}
+
+void Array_Define(int size, struct Point **points){
 	(*points) = malloc(size * sizeof(*(*points)));
 }
 
-void Array_Fill_Random(int size, struct Point* points){
+void Array_Fill_Random(int size, struct Point *points){
 	for(int i = 0; i < size; i++){
 		points[i].x = Random_Number();
 		points[i].y = Random_Number();
+		points[i].angle = 0;
 	}
 }
 
-void Array_Display(int size, struct Point* points){
+void Array_Display(int size, struct Point *points){
 	for(int i = 0; i < size; i++){
-		printf("%d- (%f, %f)\n", i + 1, points[i].x, points[i].y);
+		printf("%d- (%f, %f), angle: %f\n", i + 1, points[i].x, points[i].y, points[i].angle);
 	}
+	printf("\n");
 }
 	
 int main(){
 	srand(clock());
-	Point* points;
+	Point *points;
 	Point median;
 	int size;
 	printf("How many random points do you want to create?\n");
@@ -72,9 +90,9 @@ int main(){
 	Point_Median(size, points, &median);
 	Array_Display(size, points);
 	Point_Transpose(size, points, &median);
+	Point_Define_Angles(size, points);
 	Array_Display(size, points);
 	free(points);
 }
 /* TODO:
- * calculate the angles of every point
  * */
